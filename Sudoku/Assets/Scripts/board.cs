84cd5bf49bcbd1a44a221455dc1e6195c039f6e6
William Beaty <31560421+wbeaty@users.noreply.github.com>
2020-12-02 14:10:42 -0500

    changed constraint method
    changed constraints to be a bool array that holds whether each number can go there. Makes Sets execution time increase but makes Valid go from linear to constant. It was hoped that using constraints would speed up execution time but it does not.
    Also updated display to show how numbers can go in each cell in red and a smaller font

--- Sudoku/Assets/Scripts/board.cs
+++ Sudoku/Assets/Scripts/board.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,36 +8,44 @@ struct cell{ //one of the 81 spaces of the board, handles text updates and cell 
     public int num { // requires 1-9 values, 0 for empty
         set{//also update text
             _num = value;
-
-            if (num != 0){ //update the number for non-empty
-                text.color = (given) ? Color.black : new Color(0.41f, 0.01f, 0.46f);//color the given values differently from guesses
-                text.text = _num.ToString();
-            }
-            else { //activate contraint display
-                // text.text = ""; //for no display for empty
-                text.text = _contraintsCount.ToString();
-                text.color = Color.red;
-            }
+            UpdateDisplay();
         }
         get{
             return _num;
         }
     }
-    public int contraintsCount{
+    public int constraintsCount{
+        get {
+            int ret = 0;
+            for (int i = 0; i < _constrainedNumbers.Length; i++)
+                ret += (_constrainedNumbers[i]) ? 1 : 0;
+            return ret;
+        }
+    }
+    public bool[] constrainedNumbers{
         set{
-            _contraintsCount = value;
-
-            if (num == 0){//update display
-                text.text = _contraintsCount.ToString();
-                text.color = Color.red;
-            }
+            _constrainedNumbers = value;
+            UpdateDisplay();
         }
         get{
-            return _contraintsCount;
+            return _constrainedNumbers;
+        }
+    }
+    public void UpdateDisplay(){
+        if (num != 0){ //update the number for non-empty
+            text.color = (given) ? Color.black : new Color(0.41f, 0.01f, 0.46f);//color the given values differently from guesses
+            text.text = _num.ToString();
+            text.fontSize = 42;
+        }
+        else { //activate contraint display
+            text.text = (9 - constraintsCount).ToString();
+            text.color = Color.red;
+            text.fontSize = 25;
         }
     }
     private int _num;
-    public int _contraintsCount;//number of non-empties in the same column row and box. The higher this number, the less options for input
+    // public int _contraintsCount;//number of non-empties in the same column row and box. The higher this number, the less options for input
+    private bool[] _constrainedNumbers;//size 9. Is i+1 contrained from being put here
     public bool given; // was this a given/clue value in the puzzle (used for text coloring)
     public Text text; //number display of this space in Unity UI
 }
@@ -76,64 +85,103 @@ public class board : MonoBehaviour
         {1,2,0,0,3,4,6,0,0},
     };
     void Start(){//This is called upon creation/program start
-        InitCells();
         ResetBoard();
     }
 
     public void Set(int num, int row, int col){//sets space at position, to be accessed by solver
+        int oldNum = cells[row,col].num;
         cells[row,col].num = num;
-        UpdateContraints((num != 0), row, col);
+        UpdateContraints(num, oldNum, row, col);
     }
     
-    void UpdateContraints(bool addTo, int row, int col){//update the contraits of cells that are affected by this cell changing
+    void UpdateContraints(int num, int oldNum, int row, int col){//update the contraits of cells that are affected by this cell changing
         //check same row
         for (int i = 0; i < 9; i++)
-            if (i != col)//updates only on ones that are empty, or might be empty again if it backtracks (so all non-given)
-                cells[row, i].contraintsCount += (addTo) ? 1 : -1; //add or subtract from count
+            if (i != col){//avoids updating input cell
+                if (num != 0)//if it's inserting a new number, update the constrains around it
+                    cells[row, i].constrainedNumbers[num - 1] = true; //can be set to true regardless of current value
+                else if (oldNum != 0)//if it's removing a number and theres a constraint there, it needs to check if it's stil constrained on that number
+                    cells[row, i].constrainedNumbers[oldNum - 1] = ConstraintValidation(oldNum, row, col); //setting directly to false could make inaccurate
+                cells[row, i].UpdateDisplay();
+            }
 
         //check same column
         for (int i = 0; i < 9; i++)
-            if (i != row)//avoids updating the input cell
-                cells[i, col].contraintsCount += (addTo) ? 1 : -1;
+            if (i != row){
+                if (num != 0)
+                    cells[i, col].constrainedNumbers[num - 1] = true;
+                else if (oldNum != 0)
+                    cells[i, col].constrainedNumbers[oldNum - 1] = ConstraintValidation(oldNum, row, col);
+                cells[i, col].UpdateDisplay();
+            }
 
         //check the same box
         int boxRow = (row / 3) * 3;//find top left corner of this cells 3x3 box
         int boxCol = (col / 3) * 3;//use int truncation to round down
         for (int i = 0; i < 3; i++)
             for(int j = 0; j < 3; j++)
-                if (!(row == boxRow + i || col == boxCol + j))//check a 3x3 from an offset, avoiding ones in already updated
-                    cells[boxRow + i, boxCol + j].contraintsCount += (addTo) ? 1 : -1;
+                if (!(row == boxRow + i || col == boxCol + j)){//check a 3x3 from an offset, avoiding ones in already updated
+                    cells[boxRow + i, boxCol + j].constrainedNumbers[num - 1] = true;
+                    if (num != 0)
+                        cells[boxRow + i, boxCol + j].constrainedNumbers[num - 1] = true;
+                    else if (oldNum != 0)
+                        cells[boxRow + i, boxCol + j].constrainedNumbers[oldNum - 1] = ConstraintValidation(oldNum, row, col);
+                    cells[boxRow + i, boxCol + j].UpdateDisplay();
+                }
+    }
+
+    bool ConstraintValidation(int num, int row, int col){//is this cell otherwise constrained on this number
+        //check same row
+        for (int i = 0; i < 9; i++)
+            if (cells[row, i].num == num && i != col)//check if any number matches input
+                return true;//only need to find one match
+
+        //check same column
+        for (int i = 0; i < 9; i++)
+            if (cells[i, col].num == num && i != row)//avoids updating the input cell
+                return true;
+
+        //check the same box
+        int boxRow = (row / 3) * 3;//find top left corner of this cells 3x3 box
+        int boxCol = (col / 3) * 3;//use int truncation to round down
+        for (int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++)
+                if (cells[boxRow + i, boxCol + j].num == num && !(row == boxRow + i || col == boxCol + j))
+                    return true;
+        return false;
     }
 
     public bool Valid(int num, int row, int col){ //can you put num at row,col?
-        //check same row
-        for (int i = 0; i < 9; i++)
-            if (cells[row, i].num == num)
-                return false;//if theres a match then this input is not valid
+        if (num < 1) return false;
+        return !cells[row,col].constrainedNumbers[num-1];
+        // //check same row
+        // for (int i = 0; i < 9; i++)
+        //     if (cells[row, i].num == num)
+        //         return false;//if theres a match then this input is not valid
 
-        //check same column
-        for (int i = 0; i < 9; i++)
-            if (cells[i, col].num == num)
-                return false;
+        // //check same column
+        // for (int i = 0; i < 9; i++)
+        //     if (cells[i, col].num == num)
+        //         return false;
 
-        //check the same box
-        int boxRow = (row / 3) * 3;//find top left corner of this cells 3x3 box
-        int boxCol = (col / 3) * 3;//use int truncation to round down
-        for (int i = 0; i < 3; i++)
-            for(int j = 0; j < 3; j++)
-                if (cells[boxRow + i, boxCol + j].num == num)//check a 3x3 from an offset
-                    return false;
+        // //check the same box
+        // int boxRow = (row / 3) * 3;//find top left corner of this cells 3x3 box
+        // int boxCol = (col / 3) * 3;//use int truncation to round down
+        // for (int i = 0; i < 3; i++)
+        //     for(int j = 0; j < 3; j++)
+        //         if (cells[boxRow + i, boxCol + j].num == num)//check a 3x3 from an offset
+        //             return false;
 
-        return true;//reaches here is there where no matches found anywhere
+        // return true;//reaches here is there where no matches found anywhere
     }
 
     public (int, int) NextEmpty() { // returns the row,col postion of the next
         (int, int) ret = (-1 , -1);//-1 represents a null value
-        int highest = -10000;
+        int highest = -1;
         for (int row = 0; row < 9; row++)
             for(int col = 0; col < 9; col++)
-                if(cells[row,col].num == 0 && cells[row,col].contraintsCount > highest){//search for cell with most constraints, for least backtracking
-                    highest = cells[row,col].contraintsCount;
+                if(cells[row,col].num == 0 && cells[row,col].constraintsCount > highest){//search for cell with most constraints, for least backtracking
+                    highest = cells[row,col].constraintsCount;
                     ret = (row, col);
                 }
         return ret;
@@ -145,9 +193,9 @@ public class board : MonoBehaviour
                 int row = (i / 3) * 3 + j / 3; //remember int trunkation
                 int col = (i % 3) * 3 + j % 3; //example: middle cell is cell 4(j) in box 4(i) and is at cell[4,4], the cell bellow is cell 7(j) of box 4(i) and is cell[5,4]
                 cells[row,col].text = boxes[i].cells[j];
+                cells[row,col].constrainedNumbers = Enumerable.Repeat<bool>(false, 9).ToArray();
                 cells[row,col].num = 0;
                 cells[row,col].given = false;
-                cells[row,col].contraintsCount = 0;
             }
         }
     }
@@ -164,6 +212,14 @@ public class board : MonoBehaviour
                 if (givens[i,j] != 0){//only set givens because of constraint updates
                     cells[i,j].given = true;
                     Set(givens[i,j],i,j);// 1 to 1 assign, givens will be set up for this
+                }
+            }
+        }
+        //for updating constraint  display
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if (cells[i,j].num == 0){
+                    cells[i,j].UpdateDisplay();
                 }
             }
         }
